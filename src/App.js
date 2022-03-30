e731b216e2cc024910cca6ef7ad608f40bc80f65
James Suescun <james.steven2318@gmail.com>
2022-03-29 21:08:45 -0400

    create apiKey and define state for movie data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import Form from './components/Form.js'
 import MovieDisplay from './components/MovieDisplay.js';
 
 export default function App (){
+
+    const apiKey='d1d01b90';
+    const[ movie, setMovie ] = useState(null);
+
+    
+
     return (
         <>
             <MovieDisplay />
